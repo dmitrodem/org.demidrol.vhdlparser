81b264ce5e7e749ec6b6a5bb12c703fa415d7389
Dmitriy Dyomin <dmitrodem@gmail.com>
2022-10-13 13:00:39 +0300

    [app] component.json generation

--- app/src/main/java/org/demidrol/vhdlparser/App.java
+++ app/src/main/java/org/demidrol/vhdlparser/App.java
@@ -1,28 +1,62 @@
 package org.demidrol.vhdlparser;
 
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.json.simple.JSONObject;
 
 
 public class App {
 
-    public static void main(String[] args) throws Exception {        
-        String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
-        InputStream is = System.in;
-        if ( inputFile!=null ) is = new FileInputStream(inputFile);
-        ANTLRInputStream input = new ANTLRInputStream(is);
-        vhdlLexer lexer = new vhdlLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        vhdlParser parser = new vhdlParser(tokens);
-        ParseTree tree = parser.design_file();
-        ParseTreeWalker walker = new ParseTreeWalker();
+    public static List<Path> findFilesByExtension(Path path, String fileExtension) throws IOException {
+        if (! Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Path must be a directory");
+        }
+        List<Path> result;
+        try (var walk = Files.walk(path)) {
+            result = walk
+                .filter(Files::isRegularFile)
+                .filter(p -> p.getFileName().toString().endsWith(fileExtension))
+                .filter(p -> !p.getFileName().toString().endsWith(".in" + fileExtension))
+                .filter(p -> !p.getFileName().toString().matches("gr(l?)fpw_(p?)[0-9]_.*"))
+                .filter(p -> !p.getFileName().toString().matches("mul_unisim.*"))
+                .collect(Collectors.toList());
+        }
+        return result;
+    }
 
-        vhdlListener listener = new vhdlMyListener();
-
-        walker.walk(listener, tree);
+    public static void main(String[] args) throws Exception {
+        List<Object> components = new LinkedList<Object>();
+        var vhdlFiles = findFilesByExtension(Paths.get(args[0]), ".vhd");
+        for (var vhdlFile : vhdlFiles) {
+            System.out.println(vhdlFile);
+            var input = CharStreams.fromPath(vhdlFile);
+            var lexer = new vhdlLexer(input);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new vhdlParser(tokens);
+            var tree = parser.design_file();
+            var walker = new ParseTreeWalker();
+            var listener = new vhdlMyListener();
+            walker.walk(listener, tree);
+            components = Stream.of(components, listener.getComponents())
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        }
+        var json = new JSONObject();
+        json.put("components", components);
+        var fd = new FileWriter("components.json");
+        fd.write(json.toJSONString());
+        fd.close();
     }
 }
--- app/src/main/java/org/demidrol/vhdlparser/vhdlMyListener.java
+++ app/src/main/java/org/demidrol/vhdlparser/vhdlMyListener.java
@@ -1,61 +1,125 @@
 package org.demidrol.vhdlparser;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.misc.Interval;
+import org.demidrol.vhdlparser.vhdlParser.Component_declarationContext;
+import org.demidrol.vhdlparser.vhdlParser.Package_declarationContext;
 
 public class vhdlMyListener extends vhdlBaseListener {
+
+    // if component is inside package
+    private String package_name = null;
+
+    // if component is inside architecture (declarative, blocks, etc)
+    private String entity_name = null;
+    private String architecture_name = null;
+
+    private List<Object> components = new LinkedList<Object>();
+
     public static String getString(ParserRuleContext ctx) {
         var start = ctx.getStart().getStartIndex();
         var stop  = ctx.getStop().getStopIndex();
         var interval = new Interval(start, stop);
         return ctx.start.getInputStream().getText(interval);
     }
-    @Override public void enterComponent_declaration(vhdlParser.Component_declarationContext ctx) {
 
-        var sb = new StringBuilder();
-        var id = ctx.identifier(0);
-        sb.append("component ");
-        sb.append(id.getText());
-        var generic_clause = ctx.generic_clause();
-        var port_clause = ctx.port_clause();
-        if (generic_clause != null) {
-            List<String> generics_list = new LinkedList<>();
-            for (var g : generic_clause.generic_list().interface_constant_declaration()) {
-                List<String> generic_ids = new LinkedList<>();
-                for (var g_id : g.identifier_list().identifier()) {
-                    generic_ids.add(g_id.getText());                 
-                }
-                var g_type = g.subtype_indication().selected_name(0).identifier().getText();
-                var g_decl = String.join(",", generic_ids) + " : " + g_type;
-                if (g.VARASGN() != null) {
-                    var g_default = g.expression().getText();
-                    g_decl += (" := " + g_default);
-                }
-                generics_list.add(g_decl);
-            }
-            sb.append(" generic (" + String.join("; ", generics_list) + ")");
+    public List<Object> getComponents() {
+        return this.components;
+    }
+
+    @Override
+    public void enterPackage_declaration(Package_declarationContext ctx) {
+        this.package_name = ctx.identifier(0).getText();
+    }
+
+    @Override
+    public void exitPackage_declaration(Package_declarationContext ctx) {
+        this.package_name = null;
+    }
+
+    @Override
+    public void enterArchitecture_body(vhdlParser.Architecture_bodyContext ctx) {
+        var len = ctx.identifier().size();
+        assert (len == 2) ||
+            ((len == 3) &&
+             (ctx.identifier(0).getText().toLowerCase() == ctx.identifier(2).getText().toLowerCase()));
+        this.entity_name = ctx.identifier(1).getText().toLowerCase();
+        this.architecture_name = ctx.identifier(1).getText().toLowerCase();
+    }
+
+    @Override
+    public void exitArchitecture_body(vhdlParser.Architecture_bodyContext ctx) {
+        this.entity_name = null;
+        this.architecture_name = null;
+    }
+
+    @Override public void enterComponent_declaration(Component_declarationContext ctx) {
+
+        assert
+            (this.package_name != null) ||
+            (this.entity_name != null);
+
+        var obj = new HashMap<String, Object>();
+
+        assert ctx.identifier().size() == 1;
+        obj.put("id", ctx.identifier(0).getText());
+
+        var origin = new HashMap<String, Object>();
+        origin.put("filename", ctx.start.getInputStream().getSourceName());
+        origin.put("line_start", ctx.start.getLine());
+        origin.put("line_stop", ctx.stop.getLine());
+        if (this.package_name != null) {
+            origin.put("kind", "package");
+            origin.put("package", this.package_name);
+        } else if (this.entity_name != null) {
+            origin.put("kind", "entity");
+            origin.put("entity", this.entity_name);
+            origin.put("architecture", this.architecture_name);
         }
-        if (port_clause != null) {
-            List<String> ports_list = new LinkedList<>();
-            for (var p : port_clause.port_list().interface_port_list().interface_port_declaration()) {
-                List<String> port_ids = new LinkedList<>();
-                for (var p_id : p.identifier_list().identifier()) {
-                    port_ids.add(p_id.getText());
-                }
-                var p_dir = p.signal_mode().getText();
+        obj.put("origin", origin);
+
+        var generics = new LinkedList<Object>();
+        if (ctx.generic_clause() != null) {
+            for (var g : ctx.generic_clause().generic_list().interface_constant_declaration()) {
+                var g_type = getString(g.subtype_indication()).toLowerCase();
+                String g_default = (g.VARASGN() == null) ? null : getString(g.expression());
+                g.identifier_list().identifier().forEach(id -> {
+                        var generic = new HashMap<String, Object>();
+                        generic.put("name", id.getText());
+                        generic.put("type", g_type);
+                        generic.put("default", g_default);
+                        generics.add(generic);
+                    });
+            }
+        }
+        obj.put("generics", generics);
+
+        var ports = new LinkedList<Object>();
+        if (ctx.port_clause() != null) {
+            for (var p :
+                     ctx.port_clause()
+                     .port_list()
+                     .interface_port_list()
+                     .interface_port_declaration()) {
                 var p_type = getString(p.subtype_indication());
-                var p_decl = String.join(", ", port_ids) + " : " + p_dir + " " + p_type;
-                if (p.VARASGN() != null) {
-                    var p_default = getString(p.expression());
-                    p_decl += " := " + p_default;
-                }
-                ports_list.add(p_decl);
+                var p_direction = (p.signal_mode() == null) ? "in" : getString(p.signal_mode()).toLowerCase();
+                var p_default = (p.VARASGN() == null) ? null : getString(p.expression()).toLowerCase();
+                p.identifier_list().identifier().forEach(id -> {
+                    var port = new HashMap<String, Object>();
+                    port.put("name", id.getText());
+                    port.put("type", p_type);
+                    port.put("direction", p_direction);
+                    port.put("default", p_default);
+                    ports.add(port);
+                });
             }
-            sb.append(" port (" + String.join("; ", ports_list) + ")");
         }
-        System.out.println(sb.toString());
+        obj.put("ports", ports);
+
+        this.components.add(obj);
     }
 }
-
